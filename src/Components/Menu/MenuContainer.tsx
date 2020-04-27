166632f3249874357bd97f87b91213c2752641f0
junmile <designeryjun@gmail.com>
2020-04-27 19:14:38 +0900

    #2.35

--- src/Components/Menu/MenuContainer.tsx
+++ src/Components/Menu/MenuContainer.tsx
@@ -4,6 +4,7 @@ import { Query, Mutation } from 'react-apollo';
 import { userProfile, toggleDriving } from '../../types/api';
 import { USER_PROFILE } from '../../sharedQueries.queries';
 import TOGGLE_DRIVING from './MenuQueries';
+import { toast } from 'react-toastify';
 
 class ProfileQuery extends Query<userProfile> {}
 
@@ -14,7 +15,28 @@ class MenuContainer extends React.Component {
     return (
       <ToggleDrivingMutation
         mutation={TOGGLE_DRIVING}
-        refetchQueries={[{ query: USER_PROFILE }]}
+        // refetchQueries={[{ query: USER_PROFILE }]}
+        update={(cache, { data }) => {
+          if (data) {
+            const { ToggleDrivingMode } = data;
+            if (!ToggleDrivingMode.ok) {
+              toast.error('상태를 바꿀수 없습니다.');
+              return;
+            }
+            const query: userProfile | null = cache.readQuery({
+              query: USER_PROFILE,
+            });
+            if (query) {
+              const {
+                GetMyProfile: { user },
+              } = query;
+              if (user) {
+                user.isDriving = !user.isDriving;
+              }
+            }
+            cache.writeQuery({ query: USER_PROFILE, data: query });
+          }
+        }}
       >
         {(toggleDrivingFn) => (
           <ProfileQuery query={USER_PROFILE}>
